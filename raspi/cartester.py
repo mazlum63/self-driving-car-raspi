38477fda6699833c31e89fc3928c676146d7b55d
mazlum63 <mazlumkoyuncu63@gmail.com>
2025-07-13 14:17:24 +0200

    Fix second reverse problem

--- raspi/cartester.py
+++ raspi/cartester.py
@@ -1,26 +1,24 @@
 import RPi.GPIO as GPIO
 import time
 
-# GPIO pin tanımlamaları
-MOTOR1_IN1 = 23  # sol motor
+MOTOR1_IN1 = 23  # left motor
 MOTOR1_IN2 = 24
 
-MOTOR2_IN1 = 27  # sağ motor
+MOTOR2_IN1 = 27  # right motor
 MOTOR2_IN2 = 22
 
-# GPIO ayarları
+
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
-# Pinleri çıkış olarak ayarla
+
 GPIO.setup(MOTOR1_IN1, GPIO.OUT)
 GPIO.setup(MOTOR1_IN2, GPIO.OUT)
 GPIO.setup(MOTOR2_IN1, GPIO.OUT)
 GPIO.setup(MOTOR2_IN2, GPIO.OUT)
 
 try:
-    print("İleri hareket")
-    # İleri yönde sür
+    print("forward")
     GPIO.output(MOTOR1_IN1, GPIO.HIGH)
     GPIO.output(MOTOR1_IN2, GPIO.LOW)
 
@@ -29,8 +27,7 @@ try:
 
     time.sleep(3)
 
-    print("Geri hareket")
-    # Geri yönde sür
+    print("reverse")
     GPIO.output(MOTOR1_IN1, GPIO.LOW)
     GPIO.output(MOTOR1_IN2, GPIO.HIGH)
 
@@ -40,10 +37,10 @@ try:
     time.sleep(3)
 
 finally:
-    # Tüm pinleri kapat
+
     GPIO.output(MOTOR1_IN1, GPIO.LOW)
     GPIO.output(MOTOR1_IN2, GPIO.LOW)
     GPIO.output(MOTOR2_IN1, GPIO.LOW)
     GPIO.output(MOTOR2_IN2, GPIO.LOW)
     GPIO.cleanup()
-    print("GPIO temizlendi")
+    print("GPIO temizlendicleared")
